3517d771b64e616ec399432b2472fb8b854d5baa
HaiNam362 <91371057+HaiNam362@users.noreply.github.com>
2022-12-26 21:57:54 +0700

    commit -m

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Như Quỳnh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday 💘';
+const titleCard = 'Tặng Em';
+const contentCard = 'On the occasion of my 22nd birthday, I wish you always healthy, young and beautiful. May the luckiest, best and happiest things come to you in the new age. Happy Birthday!💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'images.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
